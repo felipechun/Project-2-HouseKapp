ad0ee454cfb9cf8320880a218da5e3738587427c
Felipe Chun <felipe.ychun@gmail.com>
2019-07-14 23:58:41 -0300

    added homepage, added login and sign up modals, working on dashboard page

--- housekapp/routes/index.js
+++ housekapp/routes/index.js
@@ -10,4 +10,9 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+// GET Dashboard page
+router.get('/dashboard', (req, res, next) => {
+  res.render('dashboard');
+});
+
 module.exports = router;
