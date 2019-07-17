bd27e594c0b1eb0c3e6d4681a67761d230ac0b96
Felipe Chun <felipe.ychun@gmail.com>
2019-07-16 23:54:24 -0300

    added edit task edit expense, created completed tasks and all expenses

--- housekapp/routes/index.js
+++ housekapp/routes/index.js
@@ -18,13 +18,8 @@ router.get('/dashboard', ensureLoggedIn(), (req, res) => {
   User.findById(_id)
     .then((user) => {
       Group.find({ _id: groupId })
-<<<<<<< HEAD
-        .then((group) => {
-          console.log(group);
-=======
         .populate('people', 'name')
         .then((group) => {
->>>>>>> cbe9e4bce8e8ca97b90c7e6d183ee8a37e29d2d3
           res.render('dashboard', { user, group });
         })
         .catch(err => console.log(err));
@@ -32,72 +27,6 @@ router.get('/dashboard', ensureLoggedIn(), (req, res) => {
     .catch(err => console.log(err));
 });
 
-<<<<<<< HEAD
-router.post('/create/group', (req, res) => {
-  const { name, people, creator } = req.body;
-  const newGroup = new Group({ name, people: [creator, people] });
-  newGroup.save()
-    .then((group) => {
-      // Faz update do usuário que originou o request para adicioná-lo ao grupo que criou
-      User.findByIdAndUpdate({ _id: req.user._id }, { groupName: group.name, groupId: group._id })
-        .then((user) => {
-          // Convida cada novo usuário para participar do grupo
-          for (let i = 1; i < group.people.length; i += 1) {
-            inviteUser(user.name, group.people[i], group._id);
-          }
-          res.redirect('/dashboard');
-        })
-        .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
-});
-
-router.post('/create/task', (req, res) => {
-  const {
-    name,
-    date,
-    value,
-    whoOwes,
-  } = req.body;
-
-  const userId = req.user._id;
-
-  const newTask = new Task({ name, date, value, paidBy: userId });
-  newTask.save()
-    .then((task) => {
-      console.log('NEW TASK', task);
-      if (task.value <= 0) {
-        User.findByIdAndUpdate({ _id: req.user._id },
-          { $push: { tasks: newTask._id } })
-          .then((user) => {
-            res.redirect('/dashboard');
-          })
-          .catch(err => console.log(err));
-      } else {
-        User.findByIdAndUpdate({ _id: req.user._id },
-          { $push: { expenses: newTask._id } })
-          .then((user) => {
-            res.redirect('/dashboard');
-          })
-          .catch(err => console.log(err));
-      }
-    })
-    .catch(err => console.log(err));
-});
-
-router.get('/edit/group/:groupId', (req, res) => {
-  const { groupId } = req.params;
-  res.render('editGroup', { groupId });
-});
-
-router.post('/edit/group/:groupId', (req, res) => {
-  console.log(req.params);
-  const { groupId } = req.params;
-  const { name, people } = req.body;
-  Group.findByIdAndUpdate(groupId, { name, $push: { people } })
-    .then(() => {
-      res.redirect('/dashboard');
-=======
 router.get('/create/group/', (req, res) => {
   res.render('createGroup');
 });
@@ -160,24 +89,10 @@ router.post('/create/task', (req, res) => {
           })
           .catch(err => console.log(err));
       }
->>>>>>> cbe9e4bce8e8ca97b90c7e6d183ee8a37e29d2d3
     })
     .catch(err => console.log(err));
 });
 
-<<<<<<< HEAD
-router.post('/edit/task/:taskId', (req, res) => {
-  console.log(req.params);
-  const { taskId } = req.params;
-  const { name, people } = req.body;
-  Task.findByIdAndUpdate(taskId, { name, $push: { people } })
-    .then(() => {
-      res.redirect('/dashboard');
-    })
-    .catch(err => console.log(err));
-});
-
-=======
 router.get('/edit/group/:groupId', (req, res) => {
   const { groupId } = req.params;
   res.render('editGroup', { groupId });
@@ -212,7 +127,6 @@ router.get('/edit/task/:taskId', (req, res) => {
   //   .catch(err => console.log(err));
 });
 
->>>>>>> cbe9e4bce8e8ca97b90c7e6d183ee8a37e29d2d3
 // editar tarefas / completar tarefas
 // const {
 //   name,
